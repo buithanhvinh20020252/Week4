df0d25547a1a590174812fa15c7c00aace001d4e
buithanhvinh20020252 <78576070+buithanhvinh20020252@users.noreply.github.com>
2021-04-16 00:08:25 +0700

    Add files via upload

---
+++ baiB14.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[100], b[100];
+	int n, i;
+	int sumA = 0;
+	int sumB = 0;
+	cin >> n;
+	cout << "Nhap cac phan tu cua A" << endl;
+	for(i=0;i<n+1;i++)
+	{
+		cin >> a[i];
+	}
+	for(i=0;i<n+1;i++)
+	{
+		sumA+=a[i];
+	}
+	cout <<  "Nhap cac phan tu cua B" << endl;
+	for(i=0;i<n;i++)
+	{
+		cin >> b[i];
+	}
+	for(i=0;i<n;i++)
+	{
+		sumB+=b[i];
+	}
+	cout << "So thua la " << sumA - sumB;
+}
